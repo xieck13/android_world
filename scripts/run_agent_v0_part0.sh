a4c3db11e16e747f91445f1a2263cad474475713
xieck13 <xieck13@gmail.com>
2024-10-16 01:55:04 +0800

    add run

---
+++ scripts/run_agent_v0_part0.sh
@@ -0,0 +1,65 @@
+easy_task_part0=AudioRecorderRecordAudio,AudioRecorderRecordAudioWithFileName,BrowserDraw,BrowserMaze,CameraTakePhoto,ClockStopWatchPausedVerify,ClockStopWatchRunning,ClockTimerEntry,ContactsAddContact,ContactsNewContactDraft,ExpenseAddSingle,ExpenseDeleteMultiple,ExpenseDeleteSingle,MarkorCreateFolder,MarkorDeleteAllNotes,MarkorDeleteNewestNote,MarkorDeleteNote,MarkorEditNote,NotesIsTodo,NotesMeetingAttendeeCount
+easy_task_part1=NotesRecipeIngredientCount,OpenAppTaskEval,RecipeAddSingleRecipe,RecipeDeleteDuplicateRecipes,RecipeDeleteMultipleRecipes,RecipeDeleteSingleRecipe,RecipeDeleteSingleWithRecipeWithNoise,RetroPlayingQueue,SimpleCalendarAddOneEventRelativeDay,SimpleCalendarAddOneEventTomorrow,SimpleCalendarAddRepeatingEvent,SimpleCalendarAnyEventsOnDate,SimpleCalendarDeleteEvents,SimpleCalendarDeleteOneEvent,SimpleCalendarEventsInNextWeek,SimpleCalendarEventsInTimeRange,SimpleCalendarFirstEventAfterStartTime,SimpleCalendarLocationOfEvent,SimpleCalendarNextEvent,SimpleDrawProCreateDrawing
+easy_task_part2=SimpleSmsReply,SimpleSmsSendClipboardContent,SportsTrackerActivitiesCountForWeek,SportsTrackerActivityDuration,SportsTrackerLongestDistanceActivity,SystemBluetoothTurnOff,SystemBluetoothTurnOffVerify,SystemBluetoothTurnOn,SystemBluetoothTurnOnVerify,SystemBrightnessMax,SystemBrightnessMaxVerify,SystemBrightnessMin,SystemBrightnessMinVerify,SystemCopyToClipboard,SystemWifiTurnOff,SystemWifiTurnOffVerify,SystemWifiTurnOn,SystemWifiTurnOnVerify,TasksDueOnDate,TasksIncompleteTasksOnDate,TurnOnWifiAndOpenApp
+
+
+# python run.py   --suite_family=android_world \
+#   --agent_name=m3a4_llava_ft \
+#   --output_path ../runs/m3a4_llava_ft \
+#   --console_port 5554 \
+#   --grpc_port 8554
+
+# python run.py   --suite_family=android_world \
+#   --agent_name=m3a4_llava_ft \
+#   --output_path ../runs/m3a4_llava_ft \
+#   --console_port 5554 \
+#   --grpc_port 8554 \
+#   --tasks ExpenseDeleteDuplicates,ExpenseDeleteDuplicates2,ExpenseDeleteMultiple,ExpenseDeleteMultiple2,ExpenseDeleteSingle,FilesDeleteFile,FilesMoveFile,MarkorAddNoteHeader,MarkorChangeNoteContent,MarkorCreateFolder,MarkorCreateNote,MarkorCreateNoteAndSms,MarkorCreateNoteFromClipboard,MarkorDeleteAllNotes,MarkorDeleteNewestNote,MarkorDeleteNote,MarkorEditNote,MarkorMergeNotes,MarkorMoveNote,MarkorTranscribeReceipt,MarkorTranscribeVideo,NotesIsTodo,NotesMeetingAttendeeCount,NotesRecipeIngredientCount,NotesTodoItemCount,OpenAppTaskEval,OsmAndFavorite,OsmAndMarker,OsmAndTrack,RecipeAddMultipleRecipes,RecipeAddMultipleRecipesFromImage,RecipeAddMultipleRecipesFromMarkor,RecipeAddMultipleRecipesFromMarkor2,RecipeAddSingleRecipe,RecipeDeleteDuplicateRecipes,RecipeDeleteDuplicateRecipes2,RecipeDeleteDuplicateRecipes3,RecipeDeleteMultipleRecipes,RecipeDeleteMultipleRecipesWithConstraint,RecipeDeleteMultipleRecipesWithNoise,RecipeDeleteSingleRecipe,RecipeDeleteSingleWithRecipeWithNoise,RetroCreatePlaylist,RetroPlayingQueue,RetroPlaylistDuration,RetroSavePlaylist,SaveCopyOfReceiptTaskEval,SimpleCalendarAddOneEvent,SimpleCalendarAddOneEventInTwoWeeks,SimpleCalendarAddOneEventRelativeDay,SimpleCalendarAddOneEventTomorrow,SimpleCalendarAddRepeatingEvent,SimpleCalendarAnyEventsOnDate,SimpleCalendarDeleteEvents,SimpleCalendarDeleteEventsOnRelativeDay,SimpleCalendarDeleteOneEvent,SimpleCalendarEventOnDateAtTime,SimpleCalendarEventsInNextWeek,SimpleCalendarEventsInTimeRange,SimpleCalendarEventsOnDate,SimpleCalendarFirstEventAfterStartTime,SimpleCalendarLocationOfEvent,SimpleCalendarNextEvent,SimpleCalendarNextMeetingWithPerson,SimpleDrawProCreateDrawing,SimpleSmsReply,SimpleSmsReplyMostRecent,SimpleSmsResend,SimpleSmsSend,SimpleSmsSendClipboardContent,SimpleSmsSendReceivedAddress,SportsTrackerActivitiesCountForWeek,SportsTrackerActivitiesOnDate,SportsTrackerActivityDuration,SportsTrackerLongestDistanceActivity,SportsTrackerTotalDistanceForCategoryOverInterval,SportsTrackerTotalDurationForCategoryThisWeek,SystemBluetoothTurnOff,SystemBluetoothTurnOffVerify,SystemBluetoothTurnOn,SystemBluetoothTurnOnVerify,SystemBrightnessMax,SystemBrightnessMaxVerify,SystemBrightnessMin,SystemBrightnessMinVerify,SystemCopyToClipboard,SystemWifiTurnOff,SystemWifiTurnOffVerify,SystemWifiTurnOn,SystemWifiTurnOnVerify,TasksCompletedTasksForDate,TasksDueNextWeek,TasksDueOnDate,TasksHighPriorityTasks,TasksHighPriorityTasksDueOnDate,TasksIncompleteTasksOnDate,TurnOffWifiAndTurnOnBluetooth,TurnOnWifiAndOpenApp,VlcCreatePlaylist,VlcCreateTwoPlaylists
+
+
+# python run.py   --suite_family=miniwob \
+#   --agent_name=m3a4_llava_ft \
+#   --output_path ../runs/m3a4_llava_ft \
+#   --console_port 5554 \
+#   --grpc_port 8554 \
+
+
+# python run.py   --suite_family=android_world \
+#   --agent_name=m3a_qwen_72b \
+#   --output_path ../runs/m3a_qwen_72b \
+#   --console_port 5554 \
+#   --grpc_port 8554 \
+#   --tasks OsmAndMarker,OsmAndTrack,RetroCreatePlaylist,RetroPlayingQueue,RetroPlaylistDuration,SimpleSmsSend,SystemCopyToClipboard,SystemWifiTurnOff,SystemWifiTurnOffVerify,SystemWifiTurnOn,SystemWifiTurnOnVerify,TasksCompletedTasksForDate
+
+
+  # python run.py   --suite_family=android_world \
+  # --agent_name=m3a4_qwen_72b \
+  # --output_path ../runs/m3a4_qwen_72b \
+  # --console_port 5554 \
+  # --grpc_port 8554 \
+
+# agent_name=m3a4_llava_ft
+# python run.py   --suite_family=android_world \
+#   --agent_name=$agent_name \
+#   --output_path ../runs/$agent_name \
+#   --console_port 5554 \
+#   --grpc_port 8554 \
+#   --tasks SystemBrightnessMaxVerify,SystemBluetoothTurnOffVerify,SystemBrightnessMinVerify
+
+
+# agent_name=m3a4_qwen_72b
+# python run.py   --suite_family=android_world \
+#   --agent_name=$agent_name \
+#   --output_path ../runs/$agent_name \
+#   --console_port 5554 \
+#   --grpc_port 8554 \
+#   --tasks SystemBrightnessMaxVerify,SystemBluetoothTurnOffVerify,SystemBrightnessMinVerify
+
+
+agent_name=m3a_gpt4o
+python run.py   --suite_family=android_world \
+  --agent_name=$agent_name \
+  --output_path ../runs/train_$agent_name \
+  --console_port 5554 \
+  --grpc_port 8554 \
+  --tasks ExpenseAddMultipleFromMarkor,ExpenseDeleteDuplicates,ExpenseDeleteDuplicates2,ExpenseDeleteMultiple2,FilesDeleteFile,FilesMoveFile,MarkorAddNoteHeader,MarkorChangeNoteContent,MarkorCreateNote,MarkorCreateNoteAndSms,MarkorCreateNoteFromClipboard,MarkorMergeNotes,MarkorMoveNote,MarkorTranscribeReceipt,MarkorTranscribeVideo,NotesTodoItemCount,OsmAndFavorite,OsmAndMarker,OsmAndTrack,RecipeAddMultipleRecipes,RecipeAddMultipleRecipesFromImage,RecipeAddMultipleRecipesFromMarkor,RecipeAddMultipleRecipesFromMarkor2,RecipeDeleteDuplicateRecipes2,RecipeDeleteDuplicateRecipes3,RecipeDeleteMultipleRecipesWithConstraint,RecipeDeleteMultipleRecipesWithNoise,RetroCreatePlaylist,RetroPlaylistDuration,RetroSavePlaylist,SaveCopyOfReceiptTaskEval,SimpleCalendarAddOneEvent,SimpleCalendarAddOneEventInTwoWeeks,SimpleCalendarDeleteEventsOnRelativeDay,SimpleCalendarEventOnDateAtTime,SimpleCalendarEventsOnDate,SimpleCalendarNextMeetingWithPerson,SimpleSmsReplyMostRecent,SimpleSmsResend,SimpleSmsSend,SimpleSmsSendReceivedAddress,SportsTrackerActivitiesOnDate,SportsTrackerTotalDistanceForCategoryOverInterval,SportsTrackerTotalDurationForCategoryThisWeek,TasksCompletedTasksForDate,TasksDueNextWeek,TasksHighPriorityTasks,TasksHighPriorityTasksDueOnDate,TurnOffWifiAndTurnOnBluetooth,VlcCreatePlaylist,VlcCreateTwoPlaylists
---
+++ scripts/run_device_0.sh
@@ -0,0 +1,2 @@
+EMULATOR_NAME=AndroidWorldAvd
+EMULATOR_NAME=AndroidWorldAvd0 &&  ~/Android/Sdk/emulator/emulator  -avd $EMULATOR_NAME -no-snapshot -grpc 8554
